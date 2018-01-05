84fb328b7fc3a9a5211ff50971c7d6a4a9a26c55
Fabio Trabucchi <ftrabucchi@eab.com>
2018-01-05 09:42:25 -0500

    revert changes made to queue field in model (moved back queue choices to admin form)

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,10 +1,14 @@
 from __future__ import unicode_literals
 
+from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 from scheduler.models import RepeatableJob, ScheduledJob
 from scheduler.forms import JobAdminForm
+
+
+QUEUES = [(key, key) for key in settings.RQ_QUEUES.keys()]
 
 
 class QueueMixin(object):
@@ -15,6 +19,11 @@ class QueueMixin(object):
         actions = super(QueueMixin, self).get_actions(request)
         del actions['delete_selected']
         return actions
+
+    def get_form(self, request, obj=None, **kwargs):
+        queue_field = self.model._meta.get_field('queue')
+        queue_field.choices = QUEUES
+        return super(QueueMixin, self).get_form(request, obj, **kwargs)
 
     def delete_model(self, request, obj):
         if hasattr(obj, 'all'):
