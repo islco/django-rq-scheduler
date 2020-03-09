98ba903fbd018538c67ed867e10e1cc2e17e26b1
Matthew Egan <matthewj.egan@hotmail.com>
2020-03-09 15:26:50 +1100

    add django3 compat

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     zip_safe=True,
     install_requires=[
         'django>=1.9.0',
-        'django-model-utils>=2.4.0',
+        'django-model-utils>=4.0.0',
         'django-rq>=1.2.0',
         'rq-scheduler==0.8.3',
         'pytz>=2015.7',
